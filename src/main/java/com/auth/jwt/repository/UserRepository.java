0731331886bdc67c9c8ff1df111d046dd1343b00
abdou-karim <abdoukarimsidibe1@gmail.com>
2021-07-15 13:33:58 +0000

    spring security

---
+++ src/main/java/com/auth/jwt/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.auth.jwt.repository;
+
+import com.auth.jwt.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    User findByUsername(String username);
+}
---
+++ src/main/java/com/auth/jwt/service/UserDetail.java
@@ -0,0 +1,22 @@
+package com.auth.jwt.service;
+
+import com.auth.jwt.model.User;
+import com.auth.jwt.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class UserDetail implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user =  userRepository.findByUsername(username);
+        return new org.springframework.security.core.userdetails.User(user.getUsername(),user.getPassword(),new ArrayList<>());
+    }
+}
